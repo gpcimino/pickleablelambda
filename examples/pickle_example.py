663a98a8c5e66ea29d5b223a2cccde1493ca8d26
Giampaolo Cimino <cimino@nurc.nato.int>
2017-01-10 12:19:19 +0100

    fix typos in names

--- examples/pickle_example.py
+++ examples/pickle_example.py
@@ -1,4 +1,4 @@
-from pickablelambda import pickable
+from pickleablelambda import pickleable
 import pickle
 
 L = lambda x: x+1
@@ -6,4 +6,4 @@ L = lambda x: x+1
 print("L(10)=" + str(L(10)))
 
 with open('lambda.pickle', 'wb') as f:
-    pickle.dump(pickable(L), f)
+    pickle.dump(pickleable(L), f)
--- tests/pickablelambda_test.py
+++ tests/pickablelambda_test.py
@@ -6,7 +6,7 @@ import types
 import unittest
 
 
-from pickablelambda import pickable
+from pickleablelambda import pickleable
 
 
 def dummy_func(x):
@@ -26,47 +26,50 @@ class TestLambdaProxy(unittest.TestCase):
 
     def test_pickle_with_lamdba_proxy(self):
         lmb = lambda x: x+1
-        pickle_bin = pickle.dumps(pickable(lmb))
+        pickle_bin = pickle.dumps(pickleable(lmb))
         lmb2 = pickle.loads(pickle_bin)
         self.assertEqual(lmb(10), lmb2(10))
         self.assertTrue(isinstance(lmb2, types.FunctionType))
 
     def test_use_lambda_proxy(self):
         lmb = lambda x: x+1
-        proxy = pickable(lmb)
+        proxy = pickleable(lmb)
         self.assertEqual(lmb(100), proxy(100))
 
     def test_use_lambda_proxy_2_args(self):
         lmb = lambda x, y: x+y
-        proxy = pickable(lmb)
+        proxy = pickleable(lmb)
         self.assertEqual(lmb(100, 200), proxy(100, 200))
 
     def test_use_lambda_second_order_equation(self):
         lmb = lambda x: 5*x**2 + 3*x + 4
-        proxy = pickable(lmb)
+        proxy = pickleable(lmb)
         self.assertEqual(lmb(2), proxy(2))
 
-    def test_make_func_pickable_not_raise_err(self):
-        proxy = pickable(dummy_func)
+    def test_make_func_pickleable_not_raise_err(self):
+        proxy = pickleable(dummy_func)
         self.assertEqual(3, proxy(2))
 
     @staticmethod
     def dummy_method(x):
         return x+1
 
-    def test_make_class_method_pickable_not_raise_err(self):
-        proxy = pickable(self.dummy_method)
+    def test_make_class_method_pickleable_not_raise_err(self):
+        proxy = pickleable(self.dummy_method)
         self.assertEqual(3, proxy(2))
 
-    def test_make_partial_func_pickable_not_raise_err(self):
+    def test_make_partial_func_pickleable_not_raise_err(self):
         partial_func = partial(dummy_func, x=2)
-        proxy = pickable(partial_func)
+        proxy = pickleable(partial_func)
         self.assertEqual(3, proxy())
 
-    def test_make_partial_func_2args_pickable_not_raise_err(self):
+    def test_make_partial_func_2args_pickleable_not_raise_err(self):
         partial_func = partial(dummy_func2, y=2)
-        proxy = pickable(partial_func)
+        proxy = pickleable(partial_func)
         self.assertEqual(3, proxy(1))
+
+
+
 
 
 if __name__ == "__main__":
